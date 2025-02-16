5582c0362101f722841620dd79e7dc1d9902e627
Vodzinskyi Roman <roma.vodzinskiy@gmail.com>
2025-02-16 22:46:07 +0200

    Add skeleton structure for main page

--- stashlist/settings.py
+++ stashlist/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from apps import core
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core'
+]
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MIDDLEWARE = [
