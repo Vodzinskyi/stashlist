ac572ab846c90bc4d904ebd685f879773e9c4e89
Vodzinskyi Roman <roma.vodzinskiy@gmail.com>
2025-02-17 22:50:02 +0200

    Add login form and authentication

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,12 +1,17 @@
 from django import forms
-from .models import User
+from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     password_confirm = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-        widgets = {
-            'password': forms.PasswordInput()
-        }
+
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+        if password != password_confirm:
+            raise forms.ValidationError("Паролі не співпадають")
+        return password_confirm
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 from .forms import UserForm
 
 
@@ -7,11 +10,10 @@ def create_user(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            if form.cleaned_data['password'] != form.cleaned_data['password_confirm']:
-                form.add_error('password_confirm', 'Паролі не співпадають')
-                return render(request, 'registration_form.html', {'form': form})
             user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
             user.save()
+            login(request, user)
             return redirect('/')
     else:
         form = UserForm()
@@ -19,3 +21,23 @@ def create_user(request):
     return render(request, 'registration_form.html', {'form': form})
 
 
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Невірний логін або пароль')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login_form.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
